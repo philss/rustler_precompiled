ff207b16bf782a98f460a61a5de9aff647454753
José Valim <jose.valim@gmail.com>
2023-02-16 19:07:41 +0100

    Update mix.exs (#50)

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule RustlerPrecompiled.MixProject do
 
   def application do
     [
-      extra_applications: [:logger, :crypto, :inets, :public_key]
+      extra_applications: [:logger, :crypto, :inets, :ssl]
     ]
   end
 
