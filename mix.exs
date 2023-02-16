fcba688c0b99937c17031003ef585ead52c14ff6
Wojtek Mach <wojtekmach@users.noreply.github.com>
2023-02-16 19:20:48 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule RustlerPrecompiled.MixProject do
 
   def application do
     [
-      extra_applications: [:logger, :crypto, :inets, :ssl]
+      extra_applications: [:logger, :inets, :ssl]
     ]
   end
 
