bd858bdfbabee1ae4d29e3471b182187fe09ca03
Philip Sampaio <philip.sampaio@gmail.com>
2024-08-28 14:26:52 -0300

    Revert "Add support for headers and dynamic base URL (#65) (#77)"
    This reverts commit ae78223763005efdf98c10b82d189c1dfd5d6497.
    
    This change is temporary, since we need to release a patch first.

--- lib/rustler_precompiled/config.ex
+++ lib/rustler_precompiled/config.ex
@@ -83,31 +83,13 @@ defmodule RustlerPrecompiled.Config do
 
   defp validate_base_url!(nil), do: raise_for_nil_field_value(:base_url)
 
-  defp validate_base_url!(base_url) when is_binary(base_url) do
-    validate_base_url!({base_url, []})
-  end
-
-  defp validate_base_url!({base_url, headers}) when is_binary(base_url) and is_list(headers) do
+  defp validate_base_url!(base_url) do
     case :uri_string.parse(base_url) do
       %{} ->
-        if Enum.all?(headers, &match?({key, value} when is_binary(key) and is_binary(value), &1)) do
-          {base_url, headers}
-        else
-          raise "`:base_url` headers for `RustlerPrecompiled` must be a list of `{binary(),binary()}`"
-        end
+        base_url
 
       {:error, :invalid_uri, error} ->
         raise "`:base_url` for `RustlerPrecompiled` is invalid: #{inspect(to_string(error))}"
-    end
-  end
-
-  defp validate_base_url!({module, function}) when is_atom(module) and is_atom(function) do
-    Code.ensure_compiled!(module)
-
-    if Kernel.function_exported?(module, function, 1) do
-      {module, function}
-    else
-      raise "`:base_url` for `RustlerPrecompiled` is a function that does not exist: `#{inspect(module)}.#{function}/1`"
     end
   end
 
